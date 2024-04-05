9c8bd10c6c255c684a6f2b22aeae310f8401c824
Noel Koljanin <82367504+nkoljanin@users.noreply.github.com>
2024-04-05 16:43:16 +0200

    Revert "Adding mapping of owner to owned tokenIds"
    This reverts commit decc48653d8d385517c0273db0579209ea82567e.

--- contracts/DvAsset.sol
+++ contracts/DvAsset.sol
@@ -19,7 +19,7 @@ contract DvAsset is Context, DeVest, ReentrancyGuard, VestingToken, IERC721, IER
 
     // Events emitted by the contract
     event purchased(address indexed customer, uint256 indexed ticketId);
-    event transferred(address indexed sender, address indexed receiver, uint256 indexed ticketId);
+    event transferred(address indexed sender, address indexed reciver, uint256 indexed ticketId);
     event offered(address indexed owner, uint256 indexed ticketId, uint256 price);
     event canceled(address indexed owner, uint256 indexed ticketId);
 
@@ -37,9 +37,6 @@ contract DvAsset is Context, DeVest, ReentrancyGuard, VestingToken, IERC721, IER
 
     // Mapping of owner addresses to token count
     mapping(address => uint256) private _balances;
-
-    // Mapping of owner address to ticket ids
-    mapping(address => uint256[]) private _ownedTokens;
 
     // for trading
     struct Offer {
@@ -108,9 +105,8 @@ contract DvAsset is Context, DeVest, ReentrancyGuard, VestingToken, IERC721, IER
         _tickets[ticketId] = to;
         _balances[_msgSender()] -= 1;
         _balances[to] += 1;
-        addToOwnedAndAllTokens(to, ticketId);
 
-    emit transferred(_msgSender(), to, ticketId);
+        emit transferred(_msgSender(), to, ticketId);
     }
 
     // Purchase ticket
@@ -142,9 +138,8 @@ contract DvAsset is Context, DeVest, ReentrancyGuard, VestingToken, IERC721, IER
 
         _tickets[ticketId] = _msgSender();
         _balances[_msgSender()] += 1;
-        addToOwnedAndAllTokens(_msgSender(), ticketId);
 
-    emit purchased(_msgSender(), ticketId);
+        emit purchased(_msgSender(), ticketId);
     }
     /**
      *  Offer ticket for sales
@@ -182,42 +177,8 @@ contract DvAsset is Context, DeVest, ReentrancyGuard, VestingToken, IERC721, IER
         emit canceled(_msgSender(), ticketId);
     }
 
-    function tokenOfOwnerByIndex(address owner, uint256 index) public view virtual returns (uint256) {
-        require(index < balanceOf(owner), "ERC721Enumerable: owner index out of bounds");
-        return _ownedTokens[owner][index];
-    }
-
     /**
-     * @dev Add a new ticket to the set of tokens owned by an address
-     */
-    function addToOwnedAndAllTokens(address to, uint256 ticketId) internal virtual {
-        // Map tokenId to owner
-        uint256 length = balanceOf(to);
-        _ownedTokens[to][length] = ticketId;
-        _ownedTokensIndex[ticketId] = length;
-    }
-
-    /**
-     * @dev Removes `tokenId` from the set of tokens owned by an address
-     */
-    function removeFromOwnedTokens(address from, uint256 ticketId) internal virtual {
-        uint256 lastTokenIndex = balanceOf(from) - 1;
-        uint256 tokenIndex = _ownedTokensIndex[ticketId];
-
-        // When the token to delete is the last token, the swap operation is unnecessary
-        if (tokenIndex != lastTokenIndex) {
-            uint256 lastTokenId = _ownedTokens[from][lastTokenIndex];
-
-            _ownedTokens[from][tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
-            _ownedTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
-        }
-
-        // This also deletes the contents at the last position of the array
-        delete _ownedTokensIndex[ticketId];
-        delete _ownedTokens[from][lastTokenIndex];
-    }
-    /**
-    * @dev Returns the Uniform Resource Identifier (URI) for `ticketId` token.
+    * @dev Returns the Uniform Resource Identifier (URI) for `tokenId` token.
      */
     function tokenURI(uint256 /*tokenId*/) external view returns (string memory){
         return _tokenURI;
