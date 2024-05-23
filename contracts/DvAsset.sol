103dcb608fc71ce308aa9e8241ae75782d419568
Noel Koljanin <82367504+nkoljanin@users.noreply.github.com>
2024-05-23 16:15:55 +0200

    1.0.10

--- contracts/DvAsset.sol
+++ contracts/DvAsset.sol
@@ -131,9 +131,6 @@ contract DvAsset is Context, DeVest, ReentrancyGuard, VestingToken, IERC721, IER
         require(tradable == false, "Trading is enabled");
         require(_msgSender() != ownerOf(totalPurchased + 1), "You already own this asset");
 
-        (uint256 fee, ) = _factory.getFee();
-        _price += fee;
-
         __allowance(_msgSender(), _price);
         __transferFrom(_msgSender(), owner(), _price);
 
@@ -150,17 +147,18 @@ contract DvAsset is Context, DeVest, ReentrancyGuard, VestingToken, IERC721, IER
 
     // Purchase asset
     function purchase(uint256 assetId) external payable takeFee virtual{
+        (uint256 fee, ) = _factory.getFee();
+
         require(direct == false, "Direct purchase is enabled");
         require(assetId < totalSupply, "Asset sold out");
         require(_msgSender() != ownerOf(assetId), "You already own this asset");
         require(isForSale(assetId), "Asset not for sale");
-
-        (uint256 fee, ) = _factory.getFee();
+        require(msg.value >= price + fee, "Not enough value provided");
 
         // check if its original asset or asset offered for sale
         if (_market[assetId].owner != address(0)) {
-            __allowance(_msgSender(), _market[assetId].price + fee);
-            _exchange(_msgSender(), _market[assetId].owner, _market[assetId].price + fee);
+            __allowance(_msgSender(), _market[assetId].price);
+            _exchange(_msgSender(), _market[assetId].owner, _market[assetId].price);
 
             // remove asset from seller
             removeFromOwnedTokens(_market[assetId].owner, assetId);
@@ -170,8 +168,8 @@ contract DvAsset is Context, DeVest, ReentrancyGuard, VestingToken, IERC721, IER
             _market[assetId] = Offer(address(0), 0);
         } else {
             require(address(0) == ownerOf(assetId), "Asset not available");
-            __allowance(_msgSender(), price + fee);
-            _payment(_msgSender(), owner(), price + fee);
+            __allowance(_msgSender(), price);
+            _payment(_msgSender(), owner(), price);
             // assigned asset to buyer
             totalPurchased++;
             // cancel preSale if all assets are sold
@@ -186,16 +184,10 @@ contract DvAsset is Context, DeVest, ReentrancyGuard, VestingToken, IERC721, IER
     }
 
     function _exchange(address buyer, address seller, uint256 _price) internal virtual {
-        (uint256 fee, ) = _factory.getFee();
-        _price += fee;
-
         __transferFrom(buyer, seller, _price);
     }
 
     function _payment(address sender, address recipient, uint256 _price) internal virtual {
-        (uint256 fee, ) = _factory.getFee();
-        _price += fee;
-
         __transferFrom(sender, recipient, _price);
     }
 
